8c8688ee1cc6eedf250563661b02091c5348fdf5
victoriamccabe <victoriamccabe96@gmail.com>
2025-03-06 13:34:11 -0700

    Update JavaScript.js

--- Academy-Cinemas/JavaScript/JavaScript.js
+++ Academy-Cinemas/JavaScript/JavaScript.js
@@ -40,22 +40,17 @@ function buyTickets(){
 
 //Display Loading... on the Buy Tickets button
 
-// Function to show "Loading..." state
 function load() {
-    // Hide the "Buy Tickets" button
+    //Hide the "Buy Tickets" button
     document.getElementById("press").style.display = "none";
 
-    // Show the "Loading..." button
+    //Show the "Loading..." button
     document.getElementById("load").style.display = "inline-block";
 
-    // After 3 seconds, run the buyTickets function
+    //Timing
     setTimeout(function() {
         buyTickets();
-        
-        // Hide the "Loading..." button after buying tickets
         document.getElementById("load").style.display = "none";
-
-        // Show the "Buy Tickets" button again (you can choose to hide it or keep it)
         document.getElementById("press").style.display = "inline-block";
-    }, 3000); // 3000 milliseconds = 3 seconds
+    }, 3000); //Milisenconds
 }
